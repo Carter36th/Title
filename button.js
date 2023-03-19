c85c55755660d3ddd7feba3949ad768c5e07bd65
Carter36th <123386389+Carter36th@users.noreply.github.com>
2023-03-19 10:51:06 +0100

    Add files via upload

---
+++ button.js
@@ -0,0 +1,22 @@
+document.getElementById("first").addEventListener("click", function(){
+    $("#Text").append("Yeah, you clicked me")
+});
+document.getElementById("second").addEventListener("click", function() {
+    document.querySelector("#first").innerHTML = "New Text"
+});
+document.getElementById("third").addEventListener("click", function(){
+    first.style.backgroundColor = "red"
+});
+
+const txt1 = document.getElementById("color");
+const btn1 = document.getElementById("submit");
+
+function fun1() {
+    third.style.backgroundColor = txt1.value;
+}
+btn1.addEventListener("click",fun1);
+
+function fun2() {
+    console.log("Clicked");
+    submit.disabled = true;
+  }
